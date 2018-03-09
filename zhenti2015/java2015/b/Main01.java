23cb4df4c2e76672ca879ad88bce5f0f39af6299
zhangbiao <2570886020@qq.com>
2018-03-09 22:52:54 +0800

    三角形面积

---
+++ zhenti2015/java2015/b/Main01.java
@@ -0,0 +1,12 @@
+package java2015.b;
+/**
+ * 2015年省赛java B组
+ * @author zhangbiao
+ *
+ */
+public class Main01 {
+	public static void main(String[] args) {
+		//面积切割法
+		System.out.println(64-16-12-8);  
+	}
+}
