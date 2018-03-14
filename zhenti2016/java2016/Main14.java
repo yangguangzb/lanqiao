afc7004699c69cec11c9d0f348eaa2543eae0f30
yangguangzb <2570886020@qq.com>
2018-03-14 10:28:30 +0800

    六角填数(dfs)

--- zhenti2016/java2016/Main14.java
+++ zhenti2016/java2016/Main14.java
@@ -1,40 +1,43 @@
 package java2016;
 public class Main14 {
-	/**
-	 * 填方格(dfs深度搜索)
-	 * @param args
-	 */
-	static int[] vis=new int[11];
-	static int[] a=new int[11];
-	static int count;
+	static int[] vis=new int[14];
+	static int[] a=new int[14];
 	public static void main(String[] args) {
-		count=0;
+		vis[1]=1;
+		vis[8]=1;
+		vis[3]=1;
+		a[1]=1;
+		a[2]=8;
+		a[12]=3;
 		dfs(1);
-		System.out.println(count);
 	}
+	
 	public static void dfs(int x){
-		if(x>10){
-			if(Math.abs(a[1]-a[2])>1 && Math.abs(a[1]-a[4])>1 && Math.abs(a[1]-a[5])>1 && Math.abs(a[1]-a[6])>1 &&  
-	           Math.abs(a[2]-a[3])>1 && Math.abs(a[2]-a[5])>1 && Math.abs(a[2]-a[6])>1 && Math.abs(a[2]-a[7])>1 &&  
-	           Math.abs(a[3]-a[6])>1 && Math.abs(a[3]-a[7])>1 && Math.abs(a[4]-a[5])>1 && Math.abs(a[4]-a[8])>1 && 
-	           Math.abs(a[4]-a[9])>1 && Math.abs(a[5]-a[6])>1 && Math.abs(a[5]-a[8])>1 && Math.abs(a[5]-a[9])>1 && 
-	           Math.abs(a[5]-a[10])>1 && Math.abs(a[6]-a[7])>1 && Math.abs(a[6]-a[9])>1 && Math.abs(a[6]-a[10])>1 &&  
-	           Math.abs(a[7]-a[10])>1 && Math.abs(a[8]-a[9])>1 &&Math.abs(a[9]-a[10])>1){
-				count++;
-				for(int i=1;i<11;i++){
-					System.out.print(a[i]);
+		if(x==1||x==2||x==12){
+			dfs(x+1);
+			return ;
+		}
+		if(x>12){
+			int[] b=new int[6];
+			b[0]=a[2]+a[3]+a[4]+a[5];
+			b[1]=a[2]+a[6]+a[9]+a[12];
+			b[2]=a[5]+a[7]+a[10]+a[12];
+			b[3]=a[1]+a[3]+a[6]+a[8];
+			b[4]=a[1]+a[4]+a[7]+a[11];
+			b[5]=a[8]+a[9]+a[10]+a[11];
+			if(b[0]==b[1]&&b[1]==b[2]&&b[2]==b[3]&&b[3]==b[4]&&b[4]==b[5]){
+				for(int i=1;i<13;i++){
+					System.out.print(a[i]+" ");
 				}
 				System.out.println();
 			}
 		}
-		
-		for(int i=0;i<10;i++){
-			if(vis[i]==0){	//该位置没有填数字
-				vis[i]=1;	//标记以填的数字
-				a[x]=i;		//赋值
-				dfs(x+1);	//填下一个数字
-				vis[i]=0;	//清空数字
-				
+		for(int j=2;j<=12;j++){
+			if(vis[j]==0){
+				vis[j]=1;
+				a[x]=j;
+				dfs(x+1);
+				vis[j]=0;
 			}
 		}
 	}
