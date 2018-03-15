febb28a56f206ddcd1af5c684be263bcf711c3e5
yangguangzb <2570886020@qq.com>
2018-03-15 11:53:27 +0800

    杨辉三角形

---
+++ zhenti2017/java2017/Main06.java
@@ -0,0 +1,32 @@
+package java2017;
+
+public class Main06 {
+	// 杨辉三角形的第row行第col列
+	static long f(int row, int col){
+		if(row<2) return 1;
+		if(col==0) return 1;
+		if(col==row) return 1;
+		
+		long[] a = new long[row+1];
+		a[0]=1;
+		a[1]=1;
+		
+		int p = 2;
+		
+		while(p<=row){
+			a[p] = 1;
+			for(int q=p;q>0;q--){
+				a[q] = a[q] + a[q-1];
+			}
+			p++;
+		}
+		
+		return a[col];
+	}
+	
+	public static void main(String[] args){
+		System.out.println(f(4,2));
+		System.out.println(f(6,3));		
+	}
+	
+}
