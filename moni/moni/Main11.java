f3b34357cd650167474819261e31db97319b2a25
yangguangzb <2570886020@qq.com>
2018-03-26 09:43:39 +0800

    加减乘dfs

--- moni/moni/Main11.java
+++ moni/moni/Main11.java
@@ -39,34 +39,3 @@ public class Main11 {
 		System.out.println(s);
 	}
 }
-
-/*
-int f[11] = {0,1,2,3,4,5,6,7,8,9,10};  
-int a_b(int a, char c, int b)  
-{  
-    if(c == '+')  
-        return a + b;  
-    else return a - b;  
-}  
-int dfs(int a, char c, int b, int cnt)  
-{  
-    if(cnt == 11)  
-    {  
-        if(a_b(a, c, b) == 0)  
-            return 1; //success;   
-        else  
-            return 0;  
-    }  
-    int res = 0;  
-    res += dfs(a_b(a,c,b), '+', f[cnt], cnt+1);  
-    res += dfs(a_b(a,c,b), '-', f[cnt], cnt+1);  
-    res += dfs(a, c, b*f[cnt], cnt+1);  
-    return res;  
-}  
-int main()  
-{  
-    printf("%d", dfs(0,'+',1,2));  
-    return 0;  
-}   
-*/
-
