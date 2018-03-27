5e1942673f21a98cd2976408a3e04afcd3e5849f
yangguangzb <2570886020@qq.com>
2018-03-27 09:13:27 +0800

    数列求值

--- moni/moni/Main15.java
+++ moni/moni/Main15.java
@@ -1,22 +1,25 @@
 package moni;
-
+/*  推到公式
+A2=2*A1-A0;
+A3=2*A2-A1=3A1-2*A0;
+A4=2*A3-A2=4*A1-3*A0;
+*/
 import java.util.Scanner;
-
 public class Main15 {
-	/*  推到公式
-	A2=2*A1-A0;
-	A3=2*A2-A1=3A1-2*A0;
-	A4=2*A3-A2=4*A1-3*A0;
-	*/
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 		double a0=sc.nextDouble();
 		double an1=sc.nextDouble();
-		double[] c=new double[n];
-		for(int i=0;i<n;i++){
+		double[] c=new double[n+1];
+		c[0]=0.0;
+		for(int i=1;i<=n;i++){
 			c[i]=sc.nextDouble();
 		}
-		System.out.println(String.format("%.2f",(an1+n*a0-2*c[n-1])/(n+1)));
+		double sum=0.0;
+		for(int i=n;i>=1;i--){
+			sum=sum+2*(i)*c[n-i+1];
+		}
+		System.out.println(String.format("%.2f",(an1+n*a0-sum)/(n+1)));
 	}
 }
